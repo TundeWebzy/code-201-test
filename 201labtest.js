ba6e667f9645ea81661aaa0a9f8ef960603d78cb
Babatunde Akeem <tundewisdom0200@gmail.com>
2023-03-28 13:51:12 +0100

    initial commit

---
+++ 201labtest.js
@@ -0,0 +1,39 @@
+let name = prompt("What is your name?");
+console.log("Question 1: What is your name?");
+console.log("Answer 1: " + name);
+
+let color = prompt("What is your favorite color?");
+console.log("Question 2: What is your favorite color?");
+console.log("Answer 2: " + color);
+
+let food = prompt("What is your favorite food?");
+console.log("Question 3: What is your favorite food?");
+console.log("Answer 3: " + food);
+
+let destination = prompt("Where is your dream vacation destination?");
+console.log("Question 4: Where is your dream vacation destination?");
+console.log("Answer 4: " + destination);
+
+alert(
+  "Greetings, " +
+    name +
+    ", I see that your favorite color is " +
+    color +
+    ", and you like to eat " +
+    food +
+    ". I hope you get to visit " +
+    destination +
+    " soon!"
+);
+
+console.log(
+  "Output message: Greetings, " +
+    name +
+    ", I see that your favorite color is " +
+    color +
+    ", and you like to eat " +
+    food +
+    ". I hope you get to visit " +
+    destination +
+    " soon!"
+);
